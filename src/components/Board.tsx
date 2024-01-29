5bafff66af7202353b78d12b03a99fe5bad650e0
Adrian Kucyk <kucykadrian2004@gmail.com>
2024-01-29 01:50:53 +0100

    fix creating full board with mines and numbers

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,66 +6,12 @@ import Timer from './Timer';
 interface IProps {
   mode: IMode;
 }
-
-const initializeBoard = ({ width, height }: IMode): number[][] => {
-  const board: number[][] = [];
-  for (let j = 0; j < width; j++) {
-    const row: number[] = [];
-    for (let i = 0; i < height; i++) {
-      row.push(0);
-    }
-    board.push(row);
-  }
-  return board;
-};
-
-const calculateStartingFields = (
-  x: number,
-  y: number,
-  height: number
-): number[] => {
-  const fields: number[] = [];
-  const neighbourOffsets = [-1, 0, 1];
-
-  for (const xOffset of neighbourOffsets) {
-    for (const yOffset of neighbourOffsets) {
-      fields.push(height * (x + xOffset) + y + yOffset);
-    }
-  }
-  return fields;
-};
-
-const startGame = (x: number, y: number, mode: IMode): number[][] => {
-  const startingFields: number[] = calculateStartingFields(x, y, mode.height);
-
-  const board: number[][] = [];
-  const mines: number[] = Array(mode.mines);
-  const max = mode.height * mode.width;
-  let random;
-  mines.map(() => {
-    do {
-      random = Math.floor(Math.random() * max - 1);
-    } while (!mines.includes(random) && !startingFields.includes(random));
-    mines.push(random);
-  });
-  let counter: number = 0;
-  for (let j = 0; j < mode.width; j++) {
-    const row: number[] = [];
-    for (let i = 0; i < mode.height; i++) {
-      if (mines.includes(counter)) {
-        row.push(-1);
-      } else {
-        row.push(0);
-      }
-      counter++;
-    }
-    board.push(row);
-  }
-  return board;
-};
-
 const onTileClick = (x: number, y: number) => {
   console.log(x, y);
+};
+
+const createBoard = ({ width, height }: IMode): number[][] => {
+  return Array.from(Array(width), () => Array(height).fill(null));
 };
 
 const Board = ({ mode }: IProps) => {
@@ -73,14 +19,107 @@ const Board = ({ mode }: IProps) => {
   // 0 -> empty
   const [isStarted, setIsStarted] = useState<boolean>(false);
   const [board, setBoard] = useState<number[][]>([]);
-  console.log(isStarted);
   useEffect(() => {
-    setBoard(initializeBoard(mode));
+    setBoard(createBoard(mode));
     setIsStarted(false);
   }, [mode]);
+  console.log(board);
+
+  const startGame = (x: number, y: number) => {
+    const newBoard = createBoard(mode);
+    const neighbourOffsets = [-1, 0, 1];
+
+    // marking starting fields
+    for (const xOffset of neighbourOffsets) {
+      for (const yOffset of neighbourOffsets) {
+        if (
+          x - xOffset >= 0 &&
+          y - yOffset >= 0 &&
+          x - xOffset < mode.width &&
+          y - yOffset < mode.height
+        ) {
+          newBoard[x - xOffset][y - yOffset] = 0;
+        }
+      }
+    }
+
+    // placing mines
+    for (let i = 0; i < mode.mines; i++) {
+      const a = Math.floor(Math.random() * (mode.width - 1));
+      const b = Math.floor(Math.random() * (mode.height - 1));
+      if (newBoard[a][b] != 0) {
+        newBoard[a][b] = -1;
+      } else {
+        i--;
+      }
+    }
+    // generating numbers
+    newBoard.map((row, x) =>
+      row.map((value, y) => {
+        if (value != -1) {
+          let sum = 0;
+
+          if (y - 1 >= 0 && newBoard[x][y - 1] === -1) {
+            sum++;
+          }
+
+          if (x - 1 >= 0 && newBoard[x - 1][y] === -1) {
+            sum++;
+          }
+
+          if (x - 1 >= 0 && y - 1 >= 0 && newBoard[x - 1][y - 1] === -1) {
+            sum++;
+          }
+
+          if (
+            x - 1 >= 0 &&
+            y + 1 < newBoard[0].length &&
+            newBoard[x - 1][y + 1] === -1
+          ) {
+            sum++;
+          }
+
+          if (y + 1 < newBoard[0].length && newBoard[x][y + 1] === -1) {
+            sum++;
+          }
+
+          if (x + 1 < newBoard.length && newBoard[x + 1][y] === -1) {
+            sum++;
+          }
+
+          if (
+            x + 1 < newBoard.length &&
+            y + 1 < newBoard[0].length &&
+            newBoard[x + 1][y + 1] === -1
+          ) {
+            sum++;
+          }
+
+          if (
+            x + 1 < newBoard.length &&
+            y - 1 >= 0 &&
+            newBoard[x + 1][y - 1] === -1
+          ) {
+            sum++;
+          }
+
+          newBoard[x][y] = sum;
+        }
+      })
+    );
+
+    setBoard(newBoard);
+    setIsStarted(true);
+  };
+
   const rows: string = `grid-rows-[${Array(mode.height)
     .fill('1fr')
     .join('_')}]`;
+  //! this comment is for tailwindcss to generate grid properly
+  // grid-rows-[1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr]
+  // grid-rows-[1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr]
+  // grid-rows-[1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr_1fr]
+
   return (
     <>
       <div className="flex flex-row gap-1">
@@ -89,15 +128,17 @@ const Board = ({ mode }: IProps) => {
         {isStarted && <Timer />}
       </div>
       <div className={cn(`grid grid-flow-col m-4`, rows)}>
-        {board.map((row, rowNumber) =>
-          row.map((_, cellNumber) => (
+        {board.map((row, x) =>
+          row.map((value, y) => (
             <div
-              key={`${rowNumber} + ${cellNumber}`}
-              data-x-y={`${rowNumber}, ${cellNumber}`}
-              className="w-8 h-8 border bg-primary cursor-pointer"
+              key={`${x} + ${y}`}
+              data-x-y={`${x}, ${y}`}
+              className={cn(
+                'w-8 h-8 border bg-primary cursor-pointer text-primary-foreground',
+                value === -1 && 'bg-secondary'
+              )}
               onClick={() =>
-                !isStarted
-                  ? (() => {setBoard(startGame(rowNumber, cellNumber, mode));}) : onTileClick(rowNumber, cellNumber)
+                !isStarted ? startGame(x, y) : onTileClick(x, y)
               }></div>
           ))
         )}
