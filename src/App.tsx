6d81042d75e219d082a8dcfdf9d7232c81cb2322
Adrian Kucyk <kucykadrian2004@gmail.com>
2024-02-01 14:43:45 +0100

    Revert "feat: added new background"
    This reverts commit c0c08b275f0d5403c138eecceba4cbfe4673d863.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import ModeSelection from './components/ModeSelection';
 import Board from './components/Board';
 import IModes from './models/IModes';
 import { ThemeProvider } from './components/theme-provider';
-import { BackgroundBeams } from './components/ui/background-beams';
 
 function App() {
   const modes: IModes = {
@@ -15,17 +14,12 @@ function App() {
   const [mode, setMode] = useState<string>('medium');
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className=" h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
-        <div className="max-w-2xl mx-auto p-4">
-          <div className="flex flex-col justify-center items-center h-screen [&>*]:z-10 ">
-            <ModeSelection
-              onModeChange={(value) => setMode(value)}
-              modes={Object.keys(modes)}
-            />
-            {!!mode && <Board mode={modes[mode]} />}
-          </div>
-        </div>
-        <BackgroundBeams />
+      <div className="flex flex-col justify-center items-center h-screen">
+        <ModeSelection
+          onModeChange={(value) => setMode(value)}
+          modes={Object.keys(modes)}
+        />
+        {!!mode && <Board mode={modes[mode]} />}
       </div>
     </ThemeProvider>
   );
