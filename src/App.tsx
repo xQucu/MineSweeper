ffae4bba4efaf79e235d8de13e5e55ed473a3dbd
Adrian Kucyk <kucykadrian2004@gmail.com>
2024-02-01 14:07:40 +0100

    feat add cool background and remove theme-toggling

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import ModeSelection from './components/ModeSelection';
 import Board from './components/Board';
 import IModes from './models/IModes';
+import { Boxes } from './components/ui/background-boxes';
 import { ThemeProvider } from './components/theme-provider';
-import { ModeToggle } from './components/mode-toggle';
 
 function App() {
   const modes: IModes = {
@@ -14,14 +14,19 @@ function App() {
 
   const [mode, setMode] = useState<string>('medium');
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ModeToggle />
-      <div className="flex flex-col justify-center items-center h-screen">
-        <ModeSelection
-          onModeChange={(value) => setMode(value)}
-          modes={Object.keys(modes)}
-        />
-        {!!mode && <Board mode={modes[mode]} />}
+    <ThemeProvider defaultTheme="dark">
+      <div className="h-full relative w-full overflow-hidden bg-slate-900 flex flex-col items-center justify-center rounded-lg">
+        <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+        <div className="z-20">
+          <Boxes />
+          <div className="flex flex-col justify-center items-center h-screen">
+            <ModeSelection
+              onModeChange={(value) => setMode(value)}
+              modes={Object.keys(modes)}
+            />
+            {!!mode && <Board mode={modes[mode]} />}
+          </div>
+        </div>
       </div>
     </ThemeProvider>
   );
