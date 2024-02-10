baf199bbd8f12442547f2be4af1fbebcd191057a
Adrian Kucyk <kucykadrian2004@gmail.com>
2024-02-10 01:05:45 +0100

    feat: add modal like behaviour on game over

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -192,82 +192,92 @@ const Board = ({ mode }: IProps) => {
         flags={flags}
         onGameRestart={restartGame}
       />
-      <AnimatePresence mode="wait">
-        <motion.div
-          layout
-          variants={{ visible: { transition: { staggerChildren: 0.002 } } }}
-          initial="hidden"
-          animate="visible"
-          exit="exit"
-          style={{
-            gridTemplateRows: `repeat(${mode.height}, minmax(0,1fr))`,
-          }}
-          className="grid grid-flow-col m-4 z-20">
-          {board.map((row, x) =>
-            row.map((box, y) => (
-              <motion.div
-                whileHover={
-                  !box.isRevealed && !box.isFlagged && gameState !== -1
-                    ? { scale: 0.9 }
-                    : {}
-                }
-                whileTap={gameState !== -1 ? { scale: 1.1 } : {}}
-                variants={{
-                  hidden: { y: 10, opacity: 0, scale: 0 },
-                  visible: {
-                    y: 0,
-                    opacity: 1,
-                    scale: 1,
-                    transition: {
-                      type: 'spring',
-                      stiffness: 1000,
-                    },
-                  },
-                  exit: { y: -10, opacity: 0 },
-                }}
-                key={`${x}${y}`}
-                data-x-y={`${x}, ${y}`}
-                className={cn(
-                  'w-8 h-8 border bg-primary font-semibold text-xl text-primary-foreground flex items-center justify-center',
-                  !box.isRevealed &&
-                    gameState !== -1 &&
-                    !box.isFlagged &&
-                    'shadow-[inset_#e4e4e7_1px_1px_1px_2px]',
-                  !box.isRevealed && 'cursor-pointer',
-
-                  box.isRevealed && box.value !== -1 && 'bg-ring',
-                  box.isRevealed && getNumberColor(box.value)
-                )}
-                onContextMenu={(e) => {
-                  e.preventDefault();
-                  if (gameState != -1 && !box.isRevealed) {
-                    setFlags((f) => (box.isFlagged ? f + 1 : f - 1));
-                    const newBoard: TBoard = JSON.parse(JSON.stringify(board));
-                    newBoard[x][y].isFlagged = !newBoard[x][y].isFlagged;
-                    setBoard(newBoard);
+      <motion.div
+        className={cn(
+          'z-20',
+          gameState === -1 ? 'bg-black opacity-70 bg-opacity-0' : 'opacity-100 '
+        )}>
+        <AnimatePresence mode="wait">
+          <motion.div
+            layout
+            variants={{ visible: { transition: { staggerChildren: 0.002 } } }}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+            style={{
+              gridTemplateRows: `repeat(${mode.height}, minmax(0,1fr))`,
+            }}
+            className="grid grid-flow-col m-4 z-20">
+            {board.map((row, x) =>
+              row.map((box, y) => (
+                <motion.div
+                  whileHover={
+                    !box.isRevealed && !box.isFlagged && gameState !== -1
+                      ? { scale: 0.9 }
+                      : {}
                   }
-                }}
-                onClick={() => {
-                  gameState == 0
-                    ? startGame(x, y)
-                    : !box.isRevealed &&
-                      gameState != -1 &&
+                  whileTap={gameState !== -1 ? { scale: 1.1 } : {}}
+                  variants={{
+                    hidden: { y: 10, opacity: 0, scale: 0 },
+                    visible: {
+                      y: 0,
+                      opacity: 1,
+                      scale: 1,
+                      transition: {
+                        type: 'spring',
+                        stiffness: 1000,
+                      },
+                    },
+                    exit: { y: -10, opacity: 0 },
+                  }}
+                  key={`${x}${y}`}
+                  data-x-y={`${x}, ${y}`}
+                  className={cn(
+                    'w-8 h-8 border bg-primary font-semibold text-xl text-primary-foreground flex items-center justify-center',
+                    !box.isRevealed &&
+                      gameState !== -1 &&
                       !box.isFlagged &&
-                      onTileClick(x, y);
-                }}>
-                {box.isFlagged && (gameState !== -1 || box.value === -1) && (
-                  <FlagTriangleRight color="blue" />
-                )}
-                {box.value === -1 && gameState == -1 && !box.isFlagged && (
-                  <Bomb color="red" />
-                )}
-                {gameState == -1 && box.isFlagged && box.value !== -1 && <X />}
-                {box.isRevealed && box.value > 0 && box.value}
-              </motion.div>
-            ))
-          )}
-        </motion.div>
-      </AnimatePresence>
+                      'shadow-[inset_#e4e4e7_1px_1px_1px_2px]',
+                    !box.isRevealed && 'cursor-pointer',
+
+                    box.isRevealed && box.value !== -1 && 'bg-ring',
+                    box.isRevealed && getNumberColor(box.value)
+                  )}
+                  onContextMenu={(e) => {
+                    e.preventDefault();
+                    if (gameState != -1 && !box.isRevealed) {
+                      setFlags((f) => (box.isFlagged ? f + 1 : f - 1));
+                      const newBoard: TBoard = JSON.parse(
+                        JSON.stringify(board)
+                      );
+                      newBoard[x][y].isFlagged = !newBoard[x][y].isFlagged;
+                      setBoard(newBoard);
+                    }
+                  }}
+                  onClick={() => {
+                    gameState == 0
+                      ? startGame(x, y)
+                      : !box.isRevealed &&
+                        gameState != -1 &&
+                        !box.isFlagged &&
+                        onTileClick(x, y);
+                  }}>
+                  {box.isFlagged && (gameState !== -1 || box.value === -1) && (
+                    <FlagTriangleRight color="blue" />
+                  )}
+                  {box.value === -1 && gameState == -1 && !box.isFlagged && (
+                    <Bomb color="red" />
+                  )}
+                  {gameState == -1 && box.isFlagged && box.value !== -1 && (
+                    <X />
+                  )}
+                  {box.isRevealed && box.value > 0 && box.value}
+                </motion.div>
+              ))
+            )}
+          </motion.div>
+        </AnimatePresence>
+      </motion.div>
     </>
   );
 };
--- src/components/GameInfo.tsx
+++ src/components/GameInfo.tsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import Timer from './Timer';
 import { Meteors } from './ui/meteors';
 import { FlagTriangleRight } from 'lucide-react';
@@ -13,26 +13,37 @@ const GameInfo = ({ gameState, flags, onGameRestart }: IProps) => {
   return (
     <>
       <div className="my-4">
-        <div className="relative">
+        <motion.div
+          layout
+          className="relative z-30"
+          animate={gameState == -1 ? { scale: 1.2, y: 100 } : {}}
+          transition={{ duration: 1 }}>
           <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
-          <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-3 h-full overflow-hidden rounded-2xl flex flex-col justify-center items-center gap-3">
+          <div className="relative shadow-xl bg-gray-900 border border-gray-800  px-4 py-3 h-full overflow-hidden rounded-2xl flex flex-col justify-center items-center gap-3 ">
             <div className="flex flex-row gap-3">
               <FlagTriangleRight /> {flags}
               <Timer gameState={gameState} />
               {/* <Trophy /> Top score here: 000 */}
             </div>
-            {gameState == -1 && (
-              <motion.button
-                className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300"
-                onClick={onGameRestart}>
-                Play again
-              </motion.button>
-            )}
+            <AnimatePresence>
+              {gameState == -1 && (
+                <motion.button
+                  initial={{ opacity: 0, scale: 0 }}
+                  animate={{ opacity: 1, scale: 1 }}
+                  exit={{ opacity: 0, scale: 0 }}
+                  className="border px-4 py-1 rounded-lg  border-gray-500 text-gray-300"
+                  onClick={onGameRestart}>
+                  {gameState === -1 && flags === 0
+                    ? 'Start Game'
+                    : 'Play Again'}
+                </motion.button>
+              )}
+            </AnimatePresence>
 
             {/* Meaty part - Meteor effect */}
             <Meteors number={20} />
           </div>
-        </div>
+        </motion.div>
       </div>
     </>
   );
